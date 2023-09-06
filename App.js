03ce12a6fff1ed34efff1afd88da0a70e9369da2
saswateesahoo <141055226+saswateesahoo@users.noreply.github.com>
2023-09-06 15:16:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+//import React from "react";
+//import Resturant from "./component/Basics/Resturant";
+//import UseState from "./component/Hooks/useState";
+//import UseEffect from "./component/Hooks/useEffect";
+//import UseReducer from "./component/Hooks/useReducer";
+
+//const App = () => {
+  //  return < Resturant />;
+//} ;
+//export default App;
+//import React from "react";
+import Todo from "./component/todoreact/todo";
+//const App = () => {
+  //return (
+    //<>
+      //< Todo/>
+    //</>
+  //)
+//}
+//export default App;
+//import React from "react";
+//import Temp from "./component/weather/temp";
+//const App = () => {
+  //return (
+    //<>
+      //<Temp/>
+    //</>
+  //)
+//}
+//export default App;
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
